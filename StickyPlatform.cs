896bb8ad4713cd96d7731debc555b7965832e94e
Endless <164898254+Endless0027@users.noreply.github.com>
2024-04-26 23:41:53 +0800

    Update StickyPlatform.cs

--- StickyPlatform.cs
+++ StickyPlatform.cs
@@ -13,7 +13,7 @@ public class StickyPlatform : MonoBehaviour
             collision .gameObject .transform .SetParent (transform);
         }
     }
-
+//获取好Player的Transform就可以跟着移动平台一起移动不会定住不动啦
 
     private void OnTriggerExit2D(Collider2D collision)
     {
